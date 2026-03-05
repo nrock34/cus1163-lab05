610584f16094b5df7d050d80a387bf7624f17d1a
Naeem <140856316+nrock34@users.noreply.github.com>
2026-03-04 23:34:34 -0500

    syntax

--- src/main/java/academy/javapro/lab05/MathCalculatorLab.java
+++ src/main/java/academy/javapro/lab05/MathCalculatorLab.java
@@ -50,11 +50,11 @@ public class MathCalculatorLab {
         Function<Integer, Integer> fib = (n) -> {
             if (n == 0) return 0;
             if (n == 1) return 1;
-            return fib(n-1) + fib(n-2)
-        }
+            return fib(n-1) + fib(n-2);
+        };
         
         return new Runnable() {
-            calc.result = fib(calc.n)
+            calc.result = fib(calc.n);
         }; // Replace this
     }
 
